63571e9b7871af0d3e9265b4ba5404e8690165b3
pramataroff <milenpramatarov@gmail.com>
2024-02-19 14:06:03 +0200

    fst try

---
+++ List creating/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace List_creating
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter list of colors: \n \"done\" for end");
+            List<string> colorList = new List<string>();
+            string input = Console.ReadLine();
+            while (input != "done")
+            {
+                 input = Console.ReadLine();
+
+                colorList.Add(input);
+            }
+            for (int i = 0; i < colorList.Count; i++)
+            {
+                Console.Write(colorList[i]);
+            }
+            foreach(string kvadratche in colorList)
+            {
+                Console.WriteLine(kvadratche);
+            }
+            Console.Write("Which element do you want to delete: " +
+            (colorList.Count - 1) + "?");
+            int index = int.Parse(Console.ReadLine());
+            
+            colorList.RemoveAt(index);
+            colorList[0] = "rozovo";
+        }
+    }
+}
